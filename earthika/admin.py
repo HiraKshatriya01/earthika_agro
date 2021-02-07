0436e1e20e08b4d1305751bf6ab3d9c506b55be2
Hira Kshatriya <hirakshatriiya@gmail.com>
2021-02-07 10:34:57 +0530

    earthika agro new project 7feb21

---
+++ earthika/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from earthika.models import *
+from django.utils.html import format_html
+
+# Register your models here.
+
+
+class EnquiryAdmin(admin.ModelAdmin):
+    model = Enquiry
+    list_display = ['first_name', 'message', 'mobile', 'product']
+admin.site.register(Enquiry, EnquiryAdmin)
+admin.site.register(Product)
+
+class Product_Type_PackageInline(admin.TabularInline):
+    model = Product_Type_Package
+    extra = 2
+
+    def thumbnail(self, obj):
+        return format_html('<img src="{}" style="width: 130px; \
+                           height: 100px"/>'.format(obj.img))
+
+class Product_Type_Admin(admin.ModelAdmin):
+    inlines = [Product_Type_PackageInline]
+    list_display = ('product_type','product',  'active')
+    # list_display = ('product', 'product_type', 'price', 'created', 'modified', 'active')
+    # search_fields = ('product_name','product_type','date_created','date_updated',)
+
+admin.site.register(Product_Type, Product_Type_Admin)
---
+++ earthika/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from choices import *
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True,blank=True,default='')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Product_Type(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True,blank=True, related_name='product_name')
+    product_type = models.CharField(max_length=100, null=True,blank=True,default='')
+    description = models.TextField(max_length=1000,null=True,blank=True,default='')
+    image = models.ImageField(default='',upload_to='media/product_type',blank=True)
+    price = models.PositiveIntegerField(blank=True, null=True)
+    rating = models.PositiveIntegerField(blank=True,null=True)
+    # price = models.PositiveIntegerField(blank=True,null=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+    def __str__(self):
+        return '{}'.format(self.product_type)
+class Product_Type_Package(models.Model):
+    product_package = models.ForeignKey(Product_Type, on_delete=models.CASCADE, null=True,blank=True, related_name='product_package')
+    package_image = models.ImageField(default='',upload_to='media/product_type',blank=True)
+    price = models.PositiveIntegerField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(blank=True, null=True)
+    unit = models.CharField(max_length=20,default='', blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+class Enquiry(models.Model):
+    first_name = models.CharField(max_length=100, null=True,blank=True,default='')
+    last_name = models.CharField(max_length=100, null=True,blank=True,default='')
+    email = models.CharField(max_length=100, null=True,blank=True,default='')
+    mobile = models.CharField(max_length=100, null=True,blank=True,default='')
+    message = models.CharField(max_length=100, null=True,blank=True,default='')
+    product = models.ForeignKey(Product_Type, on_delete=models.CASCADE, null=True,blank=True, related_name='enquiry_product')
+    status = models.IntegerField(CHOICE_ENQUIRY_STATUS,default=1)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.first_name+' '+self.last_name)
